b6200049158f405693f3161f918f369e0cbd619a
David Pratt <davidpratt512@gmail.com>
2019-01-02 03:01:02 -0700

    implemented all functionality except for operation without the config file.

---
+++ wol.py
@@ -0,0 +1,131 @@
+import argparse
+import configparser
+import re
+import socket
+
+
+MAC_REGEX = re.compile('^([0-9a-fA-F]{2}[-:.]?){5}[0-9a-fA-F]{2}$')
+DEFAULT_IP = '255.255.255.255'
+DEFAULT_PORT = 9
+CONFIG_FILE = 'wol_config'
+
+
+def main():
+    argparser = argparse.ArgumentParser(
+        description='Wake computers using the Wake-On-Lan protocol.'
+    )
+    argparser.add_argument(
+        'mac',
+        metavar='mac address',
+        help='The mac address of the computer you wish to wake.'
+    )
+    argparser.add_argument(
+        '-i',
+        metavar='ip',
+        help='The IP address to send the magic packet to '
+        f'(default {DEFAULT_IP}).'
+    )
+    argparser.add_argument(
+        '-p',
+        metavar='port',
+        type=int,
+        help='The port to send the magic packet to '
+        f'(default {DEFAULT_PORT}).'
+    )
+    argparser.add_argument(
+        '-s',
+        metavar='secureon',
+        help='The SecureOn password for the computer you wish to wake '
+        '(default None).'
+    )
+    args = argparser.parse_args()
+
+    mac = args.mac
+    ip = args.i
+    port = args.p
+    secureon = args.s
+
+    config = configparser.ConfigParser()
+    # TODO: make this optional
+    config.read(CONFIG_FILE)
+
+    if mac in config.sections():
+        # user has used an alias
+        alias = mac
+        mac = config.get(mac, 'mac')
+
+        # prioritizes command line arguments over config entries (!!!)
+        # reverts to defaults if config file is unavailable or
+        # formatted incorrectly
+        if not ip:
+            ip = config.get(alias, 'ip', fallback=DEFAULT_IP)
+        if not port:
+            port = config.getint(alias, 'port', fallback=DEFAULT_PORT)
+        if not secureon:
+            secureon = config.get(alias, 'secureon', fallback=None)
+    else:
+        # user did not use an alias
+        # revert to defaults for command line arguments
+        if not ip:
+            ip = DEFAULT_IP
+        if not port:
+            port = DEFAULT_PORT
+        # default for secureon is None
+
+    wake(mac, ip, port, secureon)
+
+
+def wake(mac, ip='255.255.255.255', port=9, secureon=None):
+    """
+    Sends a magic packet for the corresponding mac address and SecureOn
+    password.
+
+    """
+    magic_packet = make_magic(mac, secureon)
+
+    # create a UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # and allow it to broadcast
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.connect((ip, port))
+    sock.send(magic_packet)
+    sock.close()
+
+
+def make_magic(mac, secureon=None):
+    """
+    Creates a magic packet for the given mac address and SecureOn
+    password.
+
+    """
+    if not MAC_REGEX.match(mac):
+        raise ValueError(f'Invalid mac address: {mac}')
+
+    # remove any punctuation from the mac address
+    mac = clean_mac(mac)
+
+    if secureon:
+        # SecureOn passwords are formatted the same way as mac addresses
+        # so they can be 'cleaned' the same way
+        if not MAC_REGEX.match(secureon):
+            raise ValueError(f'Invalid SecureOn password: {secureon}')
+
+        secureon = clean_mac(secureon)
+        return bytes.fromhex('f'*12 + mac*16 + secureon)
+    else:
+        return bytes.fromhex('f'*12 + mac*16)
+
+
+def clean_mac(mac):
+    """
+    Removes punctuation from a mac address. The input mac address need
+    not be formatted 'neatly'.
+
+    Example: 7824-AF:3B-55.E3 -> 7824AF3B55E3
+
+    """
+    return ''.join(char for char in mac if char.isalnum())
+
+
+if __name__ == '__main__':
+    main()
