578af9260fd26398de398346565a5674312dd076
David Pratt <davidpratt512@gmail.com>
2019-01-05 03:14:30 -0700

    cleaned up code, got rid of the quit() function in favor of raising a SystemExit

--- wol.py
+++ wol.py
@@ -10,93 +10,8 @@ import socket
 MAC_REGEX = re.compile('^([0-9a-fA-F]{2}[-:.]?){5}[0-9a-fA-F]{2}$')
 DEFAULT_IP = '255.255.255.255'
 DEFAULT_PORT = 9
-# requires that the wol_config file be in the same directory as this file
-CONFIG_FILE = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'wol_config')
-
-
-def main():
-    argparser = argparse.ArgumentParser(
-        description='Wake computers using the Wake-On-Lan protocol.'
-    )
-    argparser.add_argument(
-        'mac',
-        metavar='mac address',
-        help='The mac address of the computer you wish to wake.'
-    )
-    argparser.add_argument(
-        '-i',
-        metavar='ip',
-        help='The IP address to send the magic packet to '
-        f'(default {DEFAULT_IP}).'
-    )
-    argparser.add_argument(
-        '-p',
-        metavar='port',
-        type=int,
-        help='The port to send the magic packet to '
-        f'(default {DEFAULT_PORT}).'
-    )
-    argparser.add_argument(
-        '-s',
-        metavar='secureon',
-        help='The SecureOn password for the computer you wish to wake '
-        '(default None).'
-    )
-    args = argparser.parse_args()
-
-    mac = args.mac
-    ip = args.i
-    port = args.p
-    secureon = args.s
-
-    config = configparser.ConfigParser()
-    # reading a file that is not present does not raise an error, so the
-    # the script can be ran without having to consider the case in which
-    # the config file is not present
-    config.read(CONFIG_FILE)
-
-    if mac in config.sections():
-        # user has used an alias
-        alias = mac
-        try:
-            # someone's config file is bound to be messed up...
-            mac = config.get(mac, 'mac')
-        except configparser.NoOptionError:
-            print(f'No mac address associated with {alias}.')
-            quit(1)
-
-        # prioritizes command line arguments over config entries (!!!)
-        # reverts to defaults if config file is unavailable or
-        # formatted incorrectly
-        if not ip:
-            ip = config.get(alias, 'ip', fallback=DEFAULT_IP)
-        if not port:
-            port = config.getint(alias, 'port', fallback=DEFAULT_PORT)
-        if not secureon:
-            secureon = config.get(alias, 'secureon', fallback=None)
-    else:
-        # user did not use an alias
-        # revert to defaults since options were not specified
-        # prefer defaults defined in config file over preset defaults
-        if not ip:
-            ip = config.get('DEFAULT', 'ip', fallback=DEFAULT_IP)
-        if not port:
-            port = config.getint('DEFAULT', 'port', fallback=DEFAULT_PORT)
-        if not secureon:
-            secureon = config.get('DEFAULT', 'secureon', fallback=None)
-
-    # display any errors in easy-to-read format
-    try:
-        wake(mac, ip, port, secureon)
-    except ValueError as e:
-        print(e.args[0])
-    else:
-        # no errors occurred
-        print('Magic Packet sent!'
-              f'\n\t     Mac: {mac}'
-              f'\n\t      IP: {ip}'
-              f'\n\t    Port: {port}'
-              f'\n\tSecureOn: {secureon}\n')
+# requires that the wol_config.ini file be in the same directory as this file
+CONFIG_FILE = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'wol_config.ini')
 
 
 def wake(mac, ip='255.255.255.255', port=9, secureon=None):
@@ -149,6 +64,81 @@ def clean_mac(mac):
 
     """
     return ''.join(char for char in mac if char.isalnum())
+
+
+def main():
+    argparser = argparse.ArgumentParser(
+        description='Wake computers using the Wake-On-Lan protocol.'
+    )
+    argparser.add_argument(
+        'mac',
+        metavar='mac address',
+        help='The mac address of the computer you wish to wake.'
+    )
+    argparser.add_argument(
+        '-i',
+        metavar='ip',
+        help='The IP address to send the magic packet to '
+        f'(default {DEFAULT_IP}).'
+    )
+    argparser.add_argument(
+        '-p',
+        metavar='port',
+        type=int,
+        help='The port to send the magic packet to '
+        f'(default {DEFAULT_PORT}).'
+    )
+    argparser.add_argument(
+        '-s',
+        metavar='secureon',
+        help='The SecureOn password for the computer you wish to wake '
+        '(default None).'
+    )
+    args = argparser.parse_args()
+
+    config = configparser.ConfigParser()
+    # reading a file that is not present does not raise an error, so the
+    # the script can be ran without having to consider the case in which
+    # the config file is not present
+    config.read(CONFIG_FILE)
+
+    mac = args.mac
+    ip = args.i
+    port = args.p
+    secureon = args.s
+    alias = 'DEFAULT'
+
+    if mac in config.sections():
+        # user has used an alias
+        alias = mac
+        try:
+            # someone's config file is bound to be messed up...
+            mac = config.get(mac, 'mac')
+        except configparser.NoOptionError:
+            print(f'No mac address associated with {alias}.')
+            raise SystemExit
+
+    # prioritizes command line arguments over config entries
+    # and prioritizes config defaults over built-in defaults (!!!)
+    if not ip:
+        ip = config.get(alias, 'ip', fallback=DEFAULT_IP)
+    if not port:
+        port = config.getint(alias, 'port', fallback=DEFAULT_PORT)
+    if not secureon:
+        secureon = config.get(alias, 'secureon', fallback=None)
+
+    # display any errors in easy-to-read format
+    try:
+        wake(mac, ip, port, secureon)
+    except ValueError as e:
+        print(e.args[0])
+    else:
+        # no errors occurred
+        print('Magic Packet sent!'
+              f'\n\t     Mac: {mac}'
+              f'\n\t      IP: {ip}'
+              f'\n\t    Port: {port}'
+              f'\n\tSecureOn: {secureon}\n')
 
 
 if __name__ == '__main__':
