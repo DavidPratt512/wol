9122659164087bc80cf465a1735d818cac5866bb
David Pratt <davidpratt512@gmail.com>
2020-04-18 22:19:13 -0700

    Change behavior of wake() function
    Remove all CLI functionality because of upcoming changes to configuration routine.

--- wol.py
+++ wol.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import argparse
-import configparser
+import json
 import os
 import re
 import socket
@@ -11,29 +11,38 @@ MAC_REGEX = re.compile('^([0-9a-fA-F]{2}[-:.]?){5}[0-9a-fA-F]{2}$')
 DEFAULT_IP = '255.255.255.255'
 DEFAULT_PORT = 9
 # requires that the wol_config.ini file be in the same directory as this file
-CONFIG_FILE = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'wol_config.ini')
+CONFIG_FILE = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'wol_config.ini')  # todo, change this to a json file and put it in ~/.config/wol.json
 
 
-def wake(mac, ip='255.255.255.255', port=9, secureon=None):
+def wake(*macs, ip='255.255.255.255', port=9, repeat=1):
     """
-    Sends a magic packet for the corresponding mac address and SecureOn
-    password.
+    Sends a magic packet for the corresponding MAC addresses and SecureOn
+    passwords. Add a SecureOn password to a MAC address by appending the
+    SecureOn password after the MAC address with a slash.
 
+    For example, to wake a computer with MAC address 12-34-56-AB-CD-EF and
+    SecureOn password 11-11-11-11-11-11, call
+    wake('12-34-56-AB-CD-EF/11-11-11-11-11-11')
+
+    Since the Wake-on-LAN protocol uses UDP, it may be advisable to repeat
+    the sending of magic packets. This can be done by adjusting the repeat
+    keyword argument.
     """
-    magic_packet = make_magic(mac, secureon)
+    magic_packets = [make_magic(*mac.split('/')) for mac in macs]
 
-    # create a UDP socket
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        # and allow it to broadcast
+        # allow UDP socket to broadcast
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.sendto(magic_packet, (ip, port))
+        for magic_packet in magic_packets:
+            for _ in range(repeat):
+                sock.sendto(magic_packet, (ip, port))
+                # todo: consider adding sleep here
 
 
 def make_magic(mac, secureon=None):
     """
     Creates a magic packet for the given mac address and SecureOn
     password.
-
     """
     mac = clean_mac(mac)
 
@@ -44,9 +53,9 @@ def make_magic(mac, secureon=None):
             raise ValueError(f'Invalid SecureOn password: {secureon}')
 
         secureon = clean_mac(secureon)
-        return bytes.fromhex('f'*12 + mac*16 + secureon)
+        return bytes.fromhex('F'*12 + mac*16 + secureon)
     else:
-        return bytes.fromhex('f'*12 + mac*16)
+        return bytes.fromhex('F'*12 + mac*16)
 
 
 def clean_mac(mac):
@@ -57,12 +66,11 @@ def clean_mac(mac):
     Example:
          >>> clean_mac('7824-AF:3B-55.E3')
          '7824AF3B55E3'
-
     """
     if not MAC_REGEX.match(mac):
-        raise ValueError(f'Invalid mac address: {mac}')
+        raise ValueError(f'Invalid MAC address or SecureOn password: {mac}')
 
-    return ''.join(char for char in mac if char.isalnum())
+    return ''.join(char for char in mac if char.isalnum()).upper()
 
 
 def main():
@@ -70,13 +78,14 @@ def main():
         description='Wake computers using the Wake-On-Lan protocol.'
     )
     argparser.add_argument(
-        'mac',
-        metavar='mac address',
-        help='The mac address of the computer you wish to wake.'
+        'macs',
+        nargs='*',
+        metavar='MAC/SecureOn',
+        help='The mac addresses of the computers you wish to wake. To add a SecureOn password, enter the mac address and SecureOn password separated by a "/": <MAC>/<SecureOn>.'
     )
     argparser.add_argument(
         '-i',
-        metavar='ip',
+        metavar='IP',
         help='The IP address to send the magic packet to '
         f'(default {DEFAULT_IP}).'
     )
@@ -87,58 +96,10 @@ def main():
         help='The port to send the magic packet to '
         f'(default {DEFAULT_PORT}).'
     )
-    argparser.add_argument(
-        '-s',
-        metavar='secureon',
-        help='The SecureOn password for the computer you wish to wake '
-        '(default None).'
-    )
     args = argparser.parse_args()
 
-    config = configparser.ConfigParser()
-    # reading a file that is not present does not raise an error, so the
-    # the script can be ran without having to consider the case in which
-    # the config file is not present
-    config.read(CONFIG_FILE)
 
-    # arguments automatically None if not specified in command line
-    mac = args.mac
-    ip = args.i
-    port = args.p
-    secureon = args.s
-    alias = 'DEFAULT'
 
-    if mac in config.sections():
-        # user has used an alias
-        alias = mac
-        try:
-            # someone's config file is bound to be messed up...
-            mac = config.get(alias, 'mac')
-        except configparser.NoOptionError:
-            print(f'No mac address associated with {alias}.')
-            raise SystemExit
-
-    # prioritizes command line arguments over config entries
-    # and prioritizes config defaults over built-in defaults (!!!)
-    if ip is None:
-        ip = config.get(alias, 'ip', fallback=DEFAULT_IP)
-    if port is None:
-        port = config.getint(alias, 'port', fallback=DEFAULT_PORT)
-    if secureon is None:
-        secureon = config.get(alias, 'secureon', fallback=None)
-
-    # display any errors in easy-to-read format
-    try:
-        wake(mac, ip, port, secureon)
-    except ValueError as e:
-        print(e.args[0])
-    else:
-        # no errors occurred
-        print('Magic Packet sent!'
-              f'\n     Mac: {mac}'
-              f'\n      IP: {ip}'
-              f'\n    Port: {port}'
-              f'\nSecureOn: {secureon}\n')
 
 
 if __name__ == '__main__':
