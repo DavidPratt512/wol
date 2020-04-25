60cd9c85d35cd16344b43b113ff5772861087a83
David Pratt <davidpratt512@gmail.com>
2020-04-25 01:57:05 -0700

    Update README for new configuration setup.
    Fix error if user did not create ~/.config/wol.json.
    
    Format with black.

--- README.md
+++ README.md
@@ -1,32 +1,79 @@
 # WOL - Wake On LAN
 **WOL** is a simple Python command line interface to the Wake On LAN protocol.
-You can configure common use cases in the `wol_config.ini` file.
+Default behavior can be changed and common use cases can be configured in a `.json` file.
 
-_Requires Python 3.6+_
+_Requires Python 3.7+_
 
 ## Usage 
 **WOL** is designed to be used from the command line.
-Without any optional setup, navigate to the directory which contains `wol.py` and just type `python3 wol.py <macaddress>` to send a magic packet to 255.255.255.255 port 9.
-Optionally, you may specify the IP address, port, and SecureOn password with the `-i`, `-p`, and `-s` flags, respectively.
+Without any additional setup, use `python3 wol.py 12:34:56:78:90:12` to send a magic packet for the MAC address 
+`12:34:56:78:90:12` to `255.255.255.255` port 9.
 
-### Optional Setup
-You may find it useful to have `wol` available as a system command.
-To do so, copy `wol.py` and `wol_config.ini` to the `~/bin/` directory.
-Change the shebang in the first line of `wol.py` to point to your Python 3.6+ interpreter.
-Also, rename `wol.py` to `wol`.
-Make `wol` executable by using `chmod u+x wol`.
+The full signature is
+```
+wol.py [-h] [-i ip] [-p port] [-r repeat] [-n interface] [-f file] [-q]
+       MAC/SecureOn [MAC/SecureOn ...]
+```
+Computers requiring a SecureOn password can be woken by separating the MAC address and SecureOn password with a 
+`/`.
+For example, to wake a computer with MAC address `12:34:56:78:90:12` and SecureOn password `AB:CD:EF:12:34:56`, use
+`python3 wol.py 12:34:56:78:90:12/AB:CD:EF:12:34:56`.
 
-You will find it useful to add aliases into the `wol_config.ini` file.
-Directions to do so are given in the comments of that file.
+Note that MAC addresses and SecureOn passwords do not require the `:` separator and are case insensitive.
+Valid separators are `:`, `-`, `.`, or no separator.
 
-Note that the `wol_config.ini` file is completely optional and the script will work without it present.
+## Configuration Setup
+Optionally, users may create a `.json` file to save common use cases and change default behavior.
 
-## Future Improvements
-- Allow the `wol_config.ini` file to be in another directory (say, `~/.config/`).
-- Allow input of multiple mac addresses with SecureOn passwords in the command line.
-- Add an option to read multiple mac/ip/port/SecureOn from a file (like a `.csv`).
-- Additionally, allow files to be aliased in the configuration file.
+By default, `wol.py` will look in `~/.config/` on a Unix like machine or `%userprofile%/.config/` on Windows for `wol.json`.
+Users may specify the `-f` flag to point to another `.json` file instead.
 
-Have any more ideas?
-Want to implement some ideas?
-Feel free to contribute!
+The `.json` file is meant to contain _groups_ of MAC addresses with corresponding IP addresses, ports, repeat counts,
+interfaces, and other groups.
+
+For example, to change the default behavior of `wol.py`, create `wol.json` in the directory specified above with a 
+`DEFAULT` group:
+```json
+{
+  "DEFAULT": {
+    "ip": "10.0.1.10",
+    "port": 2718,
+    "repeat": 2
+  }
+}
+```
+With the above configuration, `python3 wol.py 123456789012` will send 2 magic packets to `10.0.1.10` port 2718.
+
+Users may find it useful to create other groups:
+```json
+{
+  "all": {
+    "groups": [
+      "home",
+      "work"
+    ]
+  },
+  
+  "home": {
+    "ip": "home_ip",
+    "port": 3141,
+    "macs": [
+      "123456789012",
+      "123ABCDEF987"
+    ]
+  },
+  
+  "work": {
+    "ip": "work_ip",
+    "port": 2718,
+    "macs": [
+      "556677889900"
+    ]
+  }
+}
+```
+With the above configuration, `python3 wol.py all` will send magic packets for MAC addresses `123456789012` and 
+`123ABCDEF987` to `home_ip` port 3141 as well as the magic packet for MAC address `556677889900` to `work_ip` port 2718.
+
+Additionally an `"interface"` can be defined for any group.
+This option is useful if a users machine has multiple NICs.
--- wol.py
+++ wol.py
@@ -15,10 +15,10 @@ import socket
 class WOLConfig:
 
     DEFAULTS = {
-        'ip': '255.255.255.255',
-        'port': 9,
-        'interface': socket.gethostbyname(socket.gethostname()),
-        'repeat': 1
+        "ip": "255.255.255.255",
+        "port": 9,
+        "interface": socket.gethostbyname(socket.gethostname()),
+        "repeat": 1,
     }
 
     def __init__(self, config_file, **cli_kwargs):
@@ -28,13 +28,16 @@ class WOLConfig:
         # prefer configuration options in this order:
         # cli > alias config > default config > class defaults
         self._default_config = self.DEFAULTS.copy()
-        self._default_config.update(self._user_config.get('DEFAULT', self.DEFAULTS))
+        self._default_config.update(self._user_config.get("DEFAULT", self.DEFAULTS))
         self._cli_arguments = cli_kwargs
 
     def _init_dict(self):
-        with open(self._config_file, 'r', encoding='utf-8') as f:
-            config = json.load(f)
-        return config
+        try:
+            with open(self._config_file, "r", encoding="utf-8") as f:
+                config = json.load(f)
+            return config
+        except FileNotFoundError:
+            return {}
 
     def _process_alias(self, alias, seen=None, groups=None):
         """
@@ -50,19 +53,19 @@ class WOLConfig:
         alias_config = self._user_config.get(alias, alias)
 
         try:
-            groups.update(set(alias_config.pop('groups')))
+            groups.update(set(alias_config.pop("groups")))
         except (KeyError, AttributeError):
             pass
 
         try:
-            macs = alias_config.pop('macs')
+            macs = alias_config.pop("macs")
         except (KeyError, AttributeError):
             macs = []
 
         if isinstance(alias_config, str):  # assume alias_config is mac address
             config = self._default_config.copy()
             config.update(self._cli_arguments)
-            yield {'macs': [alias_config], **config}
+            yield {"macs": [alias_config], **config}
             return
 
         # alias_config is a dictionary
@@ -70,7 +73,7 @@ class WOLConfig:
         config.update(alias_config)
         config.update(self._cli_arguments)
         if macs:
-            yield {'macs': macs, **config}
+            yield {"macs": macs, **config}
         for group in groups - seen:
             yield from self._process_alias(group, seen=seen, groups=groups)
 
@@ -84,17 +87,18 @@ class WOLConfig:
             calls.extend(list(self._process_alias(alias)))
 
         def call_params(call_dict):
-            return tuple(v for k, v in call_dict.items() if k != 'macs')
+            return tuple(v for k, v in call_dict.items() if k != "macs")
+
         calls.sort(key=call_params)
 
         for _, group in itertools.groupby(calls, key=call_params):
             group_macs = []
             for call in group:
-                group_macs.extend(call.pop('macs'))
-            yield {'macs': group_macs, **call}  # pretty hack-y using call here
+                group_macs.extend(call.pop("macs"))
+            yield {"macs": group_macs, **call}  # pretty hack-y using call here
 
 
-def wake(*macs, ip='255.255.255.255', port=9, repeat=1, interface=None):
+def wake(*macs, ip="255.255.255.255", port=9, repeat=1, interface=None):
     """
     Sends a magic packet for the corresponding MAC addresses and SecureOn
     passwords. Add a SecureOn password to a MAC address by appending the
@@ -112,8 +116,8 @@ def wake(*macs, ip='255.255.255.255', port=9, repeat=1, interface=None):
     keyword argument.
     """
     if interface is None:
-        interface = WOLConfig.DEFAULTS.get('interface')
-    magic_packets = [make_magic(*mac.split('/')) for mac in macs]
+        interface = WOLConfig.DEFAULTS.get("interface")
+    magic_packets = [make_magic(*mac.split("/")) for mac in macs]
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.bind((interface, 0))
@@ -134,9 +138,9 @@ def make_magic(mac, secureon=None):
         # SecureOn passwords are formatted the same way as mac addresses
         # so they can be 'cleaned' the same way
         secureon = clean_mac(secureon)
-        return bytes.fromhex('F'*12 + mac*16 + secureon)
+        return bytes.fromhex("F" * 12 + mac * 16 + secureon)
     else:
-        return bytes.fromhex('F'*12 + mac*16)
+        return bytes.fromhex("F" * 12 + mac * 16)
 
 
 def clean_mac(mac):
@@ -148,81 +152,83 @@ def clean_mac(mac):
          >>> clean_mac('7824-aF:3B-55.E3')
          '7824AF3B55E3'
     """
-    mac_regex = re.compile('^([0-9a-fA-F]{2}[-:.]?){5}[0-9a-fA-F]{2}$')
+    mac_regex = re.compile("^([0-9a-fA-F]{2}[-:.]?){5}[0-9a-fA-F]{2}$")
     if not mac_regex.match(mac):
-        raise ValueError(f'Invalid MAC address or SecureOn password: {mac}')
+        raise ValueError(f"Invalid MAC address or SecureOn password: {mac}")
 
-    return ''.join(char for char in mac if char.isalnum()).upper()
+    return "".join(char for char in mac if char.isalnum()).upper()
 
 
-if __name__ == '__main__':
-    config_fp = os.path.join(os.path.expanduser('~'), '.config', 'wol.json')
+if __name__ == "__main__":
+    config_fp = os.path.join(os.path.expanduser("~"), ".config", "wol.json")
 
     argparser = argparse.ArgumentParser(
         description=__doc__,
-        epilog='Contribute at https://github.com/davidpratt512/wol.'
+        epilog="Contribute at https://github.com/davidpratt512/wol.",
     )
     argparser.add_argument(
-        'macs',
-        nargs='+',
-        metavar='MAC/SecureOn',
-        help='The mac addresses or groups of the computers you wish to wake. '
-             'To add a SecureOn password, enter the mac address and SecureOn '
-             'password separated by a "/": <MAC>/<SecureOn>. To define groups,'
-             f' create/edit the configuration file at {config_fp}.'
+        "macs",
+        nargs="+",
+        metavar="MAC/SecureOn",
+        help="The mac addresses or groups of the computers you wish to wake. "
+        "To add a SecureOn password, enter the mac address and SecureOn "
+        'password separated by a "/": <MAC>/<SecureOn>. To define groups,'
+        f" create/edit the configuration file at {config_fp}.",
     )
     argparser.add_argument(
-        '-i',
-        metavar='ip',
-        help='The IP address to send the magic packets to '
-        f'(default {WOLConfig.DEFAULTS.get("ip")}).'
+        "-i",
+        metavar="ip",
+        help="The IP address to send the magic packets to "
+        f'(default {WOLConfig.DEFAULTS.get("ip")}).',
     )
     argparser.add_argument(
-        '-p',
-        metavar='port',
+        "-p",
+        metavar="port",
         type=int,
-        help='The port to send the magic packets to '
-        f'(default {WOLConfig.DEFAULTS.get("port")}).'
+        help="The port to send the magic packets to "
+        f'(default {WOLConfig.DEFAULTS.get("port")}).',
     )
     argparser.add_argument(
-        '-r',
-        metavar='repeat',
+        "-r",
+        metavar="repeat",
         type=int,
-        help='How many times to send each magic packet '
-             f'(default {WOLConfig.DEFAULTS.get("repeat")}).'
+        help="How many times to send each magic packet "
+        f'(default {WOLConfig.DEFAULTS.get("repeat")}).',
     )
     argparser.add_argument(
-        '-n',
-        metavar='interface',
-        help='The interface to send magic packets from. Useful if there are '
-             'multiple NICs on your machine (default '
-             f'{WOLConfig.DEFAULTS.get("interface")}).'
+        "-n",
+        metavar="interface",
+        help="The interface to send magic packets from. Useful if there are "
+        "multiple NICs on your machine (default "
+        f'{WOLConfig.DEFAULTS.get("interface")}).',
     )
     argparser.add_argument(
-        '-f',
-        metavar='file',
-        help='The .json file to read configuration options from (default '
-             f'{config_fp}).'
+        "-f",
+        metavar="file",
+        help="The .json file to read configuration options from (default "
+        f"{config_fp}).",
     )
     argparser.add_argument(
-        '-q',
-        action='store_true',
-        help='Run quietly, do not print out result of program unless an error '
-             'occurred (default false).'
+        "-q",
+        action="store_true",
+        help="Run quietly, do not print out result of program unless an error "
+        "occurred (default false).",
     )
     parsed_args = vars(argparser.parse_args())
 
-    config_fp = os.path.abspath(parsed_args.pop('f') or config_fp)
-    quiet = parsed_args.pop('q')
-    macs_and_aliases = parsed_args.pop('macs')
+    config_fp = os.path.abspath(parsed_args.pop("f") or config_fp)
+    quiet = parsed_args.pop("q")
+    macs_and_aliases = parsed_args.pop("macs")
 
     # get rid of cli arguments from parsed args that were not specified by user
-    arguments = {'ip': 'i', 'port': 'p', 'repeat': 'r', 'interface': 'n'}
-    args = {arg: parsed_args.get(ab) for arg, ab in arguments.items() if parsed_args.get(ab)}
+    arguments = {"ip": "i", "port": "p", "repeat": "r", "interface": "n"}
+    args = {
+        arg: parsed_args.get(ab) for arg, ab in arguments.items() if parsed_args.get(ab)
+    }
 
     wol_config = WOLConfig(config_fp, **args)
     for wake_call in wol_config.get_config(*macs_and_aliases):
-        mac_addresses = wake_call.pop('macs')
+        mac_addresses = wake_call.pop("macs")
         try:
             wake(*mac_addresses, **wake_call)
         except ValueError as e:
@@ -230,10 +236,10 @@ if __name__ == '__main__':
         else:
             if not quiet:
                 print(
-                    'Sent!'
-                    f'\n      Mac:\n           ' +
-                    ('\n           '.join(mac_addresses)) +
-                    f'\n       IP: {wake_call.get("ip")}'
+                    "Sent!"
+                    f"\n      Mac:\n           "
+                    + ("\n           ".join(mac_addresses))
+                    + f'\n       IP: {wake_call.get("ip")}'
                     f'\n     Port: {wake_call.get("port")}'
                     f'\n   Repeat: {wake_call.get("repeat")}'
                     f'\nInterface: {wake_call.get("interface")}\n'
